4d88a975acbc618fd736ec4e92941ffe114b3651
Andy Dao <andydao10@gmail.com>
2016-09-19 23:27:26 -0600

    Implemented the getLargestAnagramGroup(filename) and additional things
    getLargestAnagramGroup(filename) has been implemented with a JUnit test
    made for it.
    
    Additional tests have been made.
    
    Added a if(array.length <= 1) check in getLargestAnagramsGroup() to
    return an empty list because we know that a list of size 1 or less
    doesn’t have any anagrams.

--- AnagramUtil.java
+++ AnagramUtil.java
@@ -28,156 +28,156 @@ import java.util.Comparator;
  */
 public class AnagramUtil {
 
-	/**
-	 * This method returns the sorted version of the input string. The sorting
-	 * must be accomplished using an insertion sort.
-	 * 
-	 * @param word
-	 * @return
-	 */
-	public static String sort(String word) {
-		// Place our string into an arrayList of Char's
-		ArrayList<String> stringArray = new ArrayList<String>();
-		// Grab each individual char at each position and add to our stringArray
-		for (int i = 0; i < word.length(); i++) {
-			stringArray.add(word.substring(i, i + 1));
-		}
-
-		// Use an insertionSort on our list of arrays
-		for (int p = 1; p < stringArray.size(); p++) {
-			// The initial state the first element, considering by itself, is
-			// sorted.
-			String temp = stringArray.get(p);
-			int j = p;
-			//
-			for (; j > 0 && temp.compareToIgnoreCase(stringArray.get(j - 1)) < 0; j--) {
-				stringArray.set(j, stringArray.get(j - 1));
-			}
-			stringArray.set(j, temp);
-		}
-
-		// Return all concatenated letters from stringArray back into a sorted
-		// string
-		String sortedString = "";
-		for (int i = 0; i < stringArray.size(); i++) {
-			sortedString = sortedString + stringArray.get(i);
-		}
-		return sortedString;
+    /**
+     * This method returns the sorted version of the input string. The sorting
+     * must be accomplished using an insertion sort.
+     * 
+     * @param word
+     * @return
+     */
+    public static String sort(String word) {
+	// Place our string into an arrayList of Char's
+	ArrayList<String> stringArray = new ArrayList<String>();
+	// Grab each individual char at each position and add to our stringArray
+	for (int i = 0; i < word.length(); i++) {
+	    stringArray.add(word.substring(i, i + 1));
 	}
 
-	/**
-	 * This generic method sorts the input array using an insertion sort and the
-	 * input Comparator object.
-	 * 
-	 * @param list
-	 * @param compartator
-	 */
-	public static <T> void insertionSort(T[] list, Comparator<? super T> compartator) {
-		for (int p = 1; p < list.length; p++) {
-			// the initial state the first element, considering by itself, is
-			// sorted.
-			T temporary = list[p];
-			int j = p;
-			// when the second loop is entered we are guaranteed that the
-			// elements in array positions 0 through
-			// p–1 have already been sorted and that we need to extend this to
-			// positions 0 to p
-			for (; j > 0 && compartator.compare(temporary, list[j - 1]) < 0; j--)
-				list[j] = list[j - 1];
-			list[j] = temporary;
-		}
+	// Use an insertionSort on our list of arrays
+	for (int p = 1; p < stringArray.size(); p++) {
+	    // The initial state the first element, considering by itself, is
+	    // sorted.
+	    String temp = stringArray.get(p);
+	    int j = p;
+	    //
+	    for (; j > 0 && temp.compareToIgnoreCase(stringArray.get(j - 1)) < 0; j--) {
+		stringArray.set(j, stringArray.get(j - 1));
+	    }
+	    stringArray.set(j, temp);
 	}
 
-	/**
-	 * This method returns true if the two input strings are anagrams of each
-	 * other, otherwise returns false.
-	 * 
-	 * @param left
-	 * @param right
-	 * @return boolean
-	 */
-	public static boolean areAnagrams(String left, String right) {
-		// First check to see if they're the same length
-		if (left.length() != right.length()) {
-			// if not, then we know automatically they're not Anagrams.
-			return false;
-		}
+	// Return all concatenated letters from stringArray back into a sorted
+	// string
+	String sortedString = "";
+	for (int i = 0; i < stringArray.size(); i++) {
+	    sortedString = sortedString + stringArray.get(i);
+	}
+	return sortedString;
+    }
 
-		// Using the sort method, pass in the left string and right string
-		if (sort(left.toLowerCase()).equals(sort(right.toLowerCase()))) {
-			// Return true if after sorted and they equal each other, they're
-			// Anagrams
-			return true;
-		} else {
-			return false;
-		}
+    /**
+     * This generic method sorts the input array using an insertion sort and the
+     * input Comparator object.
+     * 
+     * @param list
+     * @param compartator
+     */
+    public static <T> void insertionSort(T[] list, Comparator<? super T> compartator) {
+	for (int p = 1; p < list.length; p++) {
+	    // the initial state the first element, considering by itself, is
+	    // sorted.
+	    T temporary = list[p];
+	    int j = p;
+	    // when the second loop is entered we are guaranteed that the
+	    // elements in array positions 0 through
+	    // p–1 have already been sorted and that we need to extend this to
+	    // positions 0 to p
+	    for (; j > 0 && compartator.compare(temporary, list[j - 1]) < 0; j--)
+		list[j] = list[j - 1];
+	    list[j] = temporary;
+	}
+    }
+
+    /**
+     * This method returns true if the two input strings are anagrams of each
+     * other, otherwise returns false.
+     * 
+     * @param left
+     * @param right
+     * @return boolean
+     */
+    public static boolean areAnagrams(String left, String right) {
+	// First check to see if they're the same length
+	if (left.length() != right.length()) {
+	    // if not, then we know automatically they're not Anagrams.
+	    return false;
 	}
 
-	/**
-	 * This method returns the largest group of anagrams in the input array of
-	 * words, in no particular order. It returns an empty array if there are no
-	 * anagrams in the input array.
-	 * 
-	 * @return String[]
-	 */
+	// Using the sort method, pass in the left string and right string
+	if (sort(left.toLowerCase()).equals(sort(right.toLowerCase()))) {
+	    // Return true if after sorted and they equal each other, they're
+	    // Anagrams
+	    return true;
+	} else {
+	    return false;
+	}
+    }
 
-	public static String[] getLargestAnagramGroup(String[] largestAnagrams) {
+    /**
+     * This method returns the largest group of anagrams in the input array of
+     * words, in no particular order. It returns an empty array if there are no
+     * anagrams in the input array.
+     * 
+     * @return String[]
+     */
 
-		// sort all possible anagrams in our string
-		AnagramComparator cmp = new AnagramComparator();
-		insertionSort(largestAnagrams, cmp);
+    public static String[] getLargestAnagramGroup(String[] largestAnagrams) {
+	// If the array of anagrams only contains 1 or less words...
+	// We know that there isn't a pair of anagrams to get largest anagram group of.
+	if(largestAnagrams.length <= 1) {
+	    return new String[0];
+	}
+	
+	// sort all possible anagrams in our string
+	AnagramComparator cmp = new AnagramComparator();
+	insertionSort(largestAnagrams, cmp);
 
-		// Specifies the largest group of anagrams
-		ArrayList largestGroup = new ArrayList<>();
+	// Specifies the largest group of anagrams
+	ArrayList<String> largestGroup = new ArrayList<>();
 
-		for (int i = 0; i < largestAnagrams.length; i++) {
+	for (int i = 0; i < largestAnagrams.length; i++) {
 
-			// Creates a current list of the anagrams being scanned
-			ArrayList currentLargestAnagramGroup = new ArrayList<>();
-			currentLargestAnagramGroup.add(largestAnagrams[i]);
+	    // Creates a current list of the anagrams being scanned
+	    ArrayList<String> currentLargestAnagramGroup = new ArrayList<>();
+	    currentLargestAnagramGroup.add(largestAnagrams[i]);
 
-			for (int j = i + 1; j < largestAnagrams.length; j++) {
+	    for (int j = i + 1; j < largestAnagrams.length; j++) {
 
-				// Takes the first element in our list and compares it to every
-				// other elementleft in the list
-				if (areAnagrams(largestAnagrams[i], largestAnagrams[j])) {
+		// Takes the first element in our list and compares it to every
+		// other element left in the list
+		if (areAnagrams(largestAnagrams[i], largestAnagrams[j])) {
 
-					// If they're anagrams add them to currentList
-					currentLargestAnagramGroup.add(largestAnagrams[j]);
+		    // If they're anagrams add them to currentList
+		    currentLargestAnagramGroup.add(largestAnagrams[j]);
 
-					// If currentList is larger than our previous Largestgroup
-					// of anagrams then set that current list to the new largest
-					// list
-					if (currentLargestAnagramGroup.size() > largestGroup.size()) {
-						largestGroup = currentLargestAnagramGroup;
-
-					}
-
-				}
-
-			}
-
+		    // If currentList is larger than our previous Largestgroup
+		    // of anagrams then set that current list to the new largest
+		    // list
+		    if (currentLargestAnagramGroup.size() > largestGroup.size()) {
+			largestGroup = currentLargestAnagramGroup;
+		    }
 		}
-		// Return the Largestgroup as an array of strings
-		String[] result = new String[largestGroup.size()];
-		result = (String[]) largestGroup.toArray(result);
-
-		return result;
-
+	    }
 	}
+	// Return the Largestgroup as an array of strings
+	String[] result = new String[largestGroup.size()];
+	result = (String[]) largestGroup.toArray(result);
 
-	/**
-	 * Behaves the same as the previous method, but reads the list of words from
-	 * the input filename. It is assumed that the file contains one word per
-	 * line. If the file does not exist or is empty, the method returns an empty
-	 * array because there are no anagrams.
-	 * 
-	 * @param filename
-	 * @return String
-	 */
-	public static String[] getLargestAnagramGroup(String filename) {
-		return null;
+	return result;
 
-	}
+    }
+
+    /**
+     * Behaves the same as the previous method, but reads the list of words from
+     * the input filename. It is assumed that the file contains one word per
+     * line. If the file does not exist or is empty, the method returns an empty
+     * array because there are no anagrams.
+     * 
+     * @param filename
+     * @return String
+     */
+    public static String[] getLargestAnagramGroup(String filename) {
+	return getLargestAnagramGroup(AnagramTester.readFile(filename));
+    }
 
 }
--- AnagramUtilTest.java
+++ AnagramUtilTest.java
@@ -6,176 +6,99 @@ import java.util.Arrays;
 import java.util.Comparator;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AnagramUtilTest {
 
-	@Before
-	public void setUp() throws Exception {
+    
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testSort() {
+	assertEquals("abd", AnagramUtil.sort("dab"));
+	assertEquals("abcr", AnagramUtil.sort("crab"));
+	assertEquals("emoss", AnagramUtil.sort("moses"));
+	assertEquals("Adny", AnagramUtil.sort("Andy"));
+	assertEquals("eggr", AnagramUtil.sort("greg"));
+	assertEquals("adm", AnagramUtil.sort("dam"));
+	assertEquals("agiiinrV", AnagramUtil.sort("Virginia"));
+	assertEquals("aABCDdEFGHIJKLMNnOPQRSTUVWXYZ", AnagramUtil.sort("ZYXWVUTSRQPONMLKJIHGFEDCBandA"));
+	assertEquals("ABCDEFGHIJKLMNOPQRSTUVWXYZ", AnagramUtil.sort("ZYXWVUTSRQPONMLKJIHGFEDCBA"));
+
+
+    }
+
+    @Test
+    public void testAreAnagrams() {
+	assertTrue(AnagramUtil.areAnagrams("actress", "casters"));
+    }
+
+    @Test
+    public void testAreAnagramsIgnoreCaps() {
+	assertTrue(AnagramUtil.areAnagrams("aLLerGy", "lArgELY"));
+    }
+
+    @Test
+    public void testAreNotAnagrams() {
+	// This returns false because salesman has an extra "a" (total of 2) unlike lameness with 1 "a'
+	assertFalse(AnagramUtil.areAnagrams("salesman", "lameness")); 
+    }
+
+    
+    @Ignore
+    @Test
+    public void testInsertionSort() {
+	AnagramComparator insertionSortCompare = new AnagramComparator();
+	String[] list = { "Ace", "bad", "Foggry", "Even", "Froggy", "dab" };
+	String[] numberedList = { "5", "5", "1", "44", "353", "2", "153", "74", "1", "22", "74" };
+	AnagramUtil.insertionSort(numberedList, insertionSortCompare);
+	AnagramUtil.insertionSort(list, insertionSortCompare);
+	for (int i = 0; i < list.length; i++) {
+	    System.out.println(list[i]);
 	}
 
-	@After
-	public void tearDown() throws Exception {
+	for (int i = 0; i < numberedList.length; i++) {
+	    System.out.println(numberedList[i]);
 	}
 
-	@Test
-	public void testSort() {
-		assertEquals("abd", AnagramUtil.sort("dab"));
-		assertEquals("abcr", AnagramUtil.sort("crab"));
-		assertEquals("emoss", AnagramUtil.sort("moses"));
-		assertEquals("Adny", AnagramUtil.sort("Andy"));
-		assertEquals("eggr", AnagramUtil.sort("greg"));
-		assertEquals("adm", AnagramUtil.sort("dam"));
-		assertEquals("agiiinrV", AnagramUtil.sort("Virginia"));
-		assertEquals("aABCDdEFGHIJKLMNnOPQRSTUVWXYZ", AnagramUtil.sort("ZYXWVUTSRQPONMLKJIHGFEDCBandA"));
-		assertEquals("ABCDEFGHIJKLMNOPQRSTUVWXYZ", AnagramUtil.sort("ZYXWVUTSRQPONMLKJIHGFEDCBA"));
+    }
+    
+    
+    @Test
+    public void getLargestAnagramGroupString_NoPairs() {
+	String[] list = { "Salesman" }; // An array with only 1 word has no group of anagrams
+	String[] resultArray = AnagramUtil.getLargestAnagramGroup(list); // So should return an empty array
+	assertTrue(resultArray.length == 0);
+    }
 
-	}
-
-	@Test
-	public void testInsertionSort() {
-		AnagramComparator insertionSortCompare = new AnagramComparator();
-		String[] list = { "Ace", "bad", "Foggry", "Even", "Froggy", "dab" };
-		String[] numberedList = { "5", "5", "1", "44", "353", "2", "153", "74", "1", "22", "74" };
-		AnagramUtil.insertionSort(numberedList, insertionSortCompare);
-		AnagramUtil.insertionSort(list, insertionSortCompare);
-		for (int i = 0; i < list.length; i++) {
-			System.out.println(list[i]);
-		}
-
-		for (int i = 0; i < numberedList.length; i++) {
-			System.out.println(numberedList[i]);
-		}
-
-	}
-
-	@Test
-	public void testAreAnagrams() {
-		assertTrue(AnagramUtil.areAnagrams("actress", "casters"));
-		assertTrue(AnagramUtil.areAnagrams("Foggry", "Froggy"));
-		assertTrue(AnagramUtil.areAnagrams("bad", "dab"));
-
-	}
-
-	@Test
-	public void testAreAnagramsIgnoreCaps() {
-		assertTrue(AnagramUtil.areAnagrams("aLLerGy", "lArgELY"));
-		assertTrue(AnagramUtil.areAnagrams("FlIp", "PfLi"));
-	}
-
-	@Test
-	public void testAreNotAnagrams() {
-		assertFalse(AnagramUtil.areAnagrams("salesman", "lameness")); // This is
-																		// false
-																		// because
-																		// salesman
-																		// has
-																		// an
-																		// extra
-																		// "a"
-																		// (total
-																		// of 2)
-																		// unlike
-																		// lameness
-																		// with
-																		// 1 "a'
-		assertFalse(AnagramUtil.areAnagrams("dudeman", "dudesman"));
-	}
-
-	@Test
-	public void testGetLargestAnagramGroupStringArray() {
-		String[] word = { "alert", "aa", "later", "AA", "bzy", "yzb", "alter", "x", "y" };
-		// Removed duplicates and tested anagrams for strings listed on
-		// http://www.english-for-students.com/Complete-List-of-Anagrams.html
-		// there are two groups of 7 set anagram pairs... this only outputs one
-		String[] answers = { "abed", "bade", "bead", "abet", "beat", "beta", "abets", "baste", "betas", "beast",
-				"beats", "abut", "tabu", "tuba", "acme", "came", "mace", "acre", "care", "race", "acres", "cares",
-				"races", "scare", "actors", "costar", "castor", "actress", "casters", "recasts", "airmen", "marine",
-				"remain", "alert", "alter", "later", "alerted", "altered", "related", "treadle", "ales", "leas", "sale",
-				"seal", "aligned", "dealing", "leading", "allergy", "gallery", "largely", "regally", "amen", "mane",
-				"mean", "name", "anew", "wane", "wean", "angel", "angle", "glean", "antler", "learnt", "rental", "apt",
-				"pat", "tap", "arches", "chaser", "search", "are", "ear", "era", "arm", "mar", "ram", "arrest",
-				"rarest", "raters", "starer", "artist", "strait", "traits", "arts", "rats", "star", "tars", "arcs",
-				"cars", "scar", "ascent", "secant", "stance", "ascot", "coats", "coast", "tacos", "asleep", "elapse",
-				"please", "asp", "pas", "sap", "spa", "aspired", "despair", "diapers", "praised", "asps", "pass",
-				"saps", "spas", "assert", "asters", "stares", "aster", "rates", "stare", "taser", "tears", "ate", "eat",
-				"eta", "tea", "aridest", "astride", "staider", "tardies", "tirades", "auctioned", "cautioned",
-				"education", "awls", "laws", "slaw", "baker", "brake", "break", "bard", "brad", "drab", "bared",
-				"beard", "bread", "debar", "barely", "barley", "bleary", "bats", "stab", "tabs", "begin", "being",
-				"binge", "below", "bowel", "elbow", "bleats", "stable", "tables", "bluest", "bluets", "bustle",
-				"sublet", "subtle", "bores", "robes", "sober", "brag", "garb", "grab", "calipers", "replicas",
-				"spiracle", "caller", "cellar", "recall", "canter", "nectar", "recant", "trance", "canters", "nectars",
-				"recants", "scanter", "trances", "capes", "paces", "space", "caret", "cater", "crate", "trace",
-				"carets", "caters", "caster", "crates", "reacts", "recast", "traces", "catered", "created", "reacted",
-				"cider", "cried", "dicer", "claimed", "decimal", "declaim", "medical", "claps", "clasp", "scalp",
-				"code", "coed", "deco", "construe", "counters", "recounts", "trounces", "corset", "escort", "sector",
-				"cratered", "retraced", "terraced", "cruel", "lucre", "ulcer", "dale", "deal", "lade", "lead", "dare",
-				"dear", "read", "danger", "gander", "garden", "ranged", "darters", "retards", "starred", "traders",
-				"dater", "rated", "trade", "tread", "daters", "trades", "treads", "stared", "dates", "sated", "stead",
-				"dearth", "hatred", "thread", "dearths", "hardest", "hatreds", "threads", "trashed", "decanter",
-				"cantered", "recanted", "deigns", "design", "signed", "singed", "deist", "diets", "edits", "sited",
-				"tides", "demerit", "merited", "mitered", "denter", "rented", "tender", "deltas", "lasted", "slated",
-				"demo", "dome", "mode", "desert", "deters", "rested", "detail", "dilate", "tailed", "detains",
-				"instead", "sainted", "stained", "detour", "routed", "toured", "diaper", "paired", "repaid", "diet",
-				"edit", "tide", "tied", "direst", "driest", "stride", "discounter", "introduces", "reductions", "does",
-				"dose", "odes", "doters", "sorted", "stored", "dowry", "rowdy", "wordy", "drapes", "padres", "parsed",
-				"rasped", "spared", "spread", "drawer", "redraw", "reward", "warder", "warred", "dues", "sued", "used",
-				"duster", "rudest", "rusted", "earned", "endear", "neared", "earnest", "eastern", "nearest", "earns",
-				"nears", "saner", "snare", "ears", "eras", "sear", "earth", "hater", "heart", "east", "eats", "sate",
-				"seat", "teas", "elan", "lane", "lean", "emigrants", "mastering", "streaming", "emoter", "meteor",
-				"remote", "enlarge", "general", "gleaner", "emit", "item", "mite", "time", "emits", "items", "mites",
-				"smite", "times", "endive", "envied", "veined", "enlist", "inlets", "listen", "silent", "tinsel",
-				"enters", "nester", "resent", "tenser", "eons", "nose", "ones", "esprit", "priest", "sprite", "stripe",
-				"esprits", "persist", "spriest", "sprites", "stripes", "ester", "reset", "steer", "terse", "trees",
-				"ether", "there", "three", "evil", "live", "veil", "vile", "filets", "itself", "stifle", "filter",
-				"lifter", "trifle", "flow", "fowl", "wolf", "forest", "fortes", "foster", "softer", "glare", "lager",
-				"large", "regal", "gilder", "girdle", "glider", "gnus", "guns", "snug", "sung", "goer", "gore", "ogre",
-				"gory", "gyro", "orgy", "gust", "guts", "tugs", "hare", "hear", "rhea", "hares", "hears", "rheas",
-				"share", "shear", "heaps", "phase", "shape", "heros", "hoers", "horse", "shore", "hoes", "hose", "shoe",
-				"hops", "posh", "shop", "ideals", "ladies", "sailed", "inks", "sink", "skin", "ires", "rise", "sire",
-				"kale", "lake", "leak", "lair", "liar", "rail", "lame", "male", "meal", "lameness", "nameless",
-				"salesman", "lament", "mantel", "mantle", "mental", "lapse", "leaps", "pales", "peals", "pleas",
-				"sepal", "last", "salt", "slat", "late", "tale", "teal", "leap", "pale", "peal", "plea", "leapt",
-				"petal", "plate", "pleat", "least", "slate", "stale", "steal", "tales", "teals", "lemons", "melons",
-				"solemn", "limes", "miles", "slime", "smile", "list", "silt", "slit", "lips", "lisp", "slip", "lisper",
-				"perils", "pliers", "lister", "liters", "litres", "relist", "tilers", "livers", "silver", "sliver",
-				"loop", "polo", "pool", "looped", "poodle", "pooled", "loops", "polos", "pools", "sloop", "spool",
-				"lopes", "poles", "slope", "lots", "lost", "slot", "lusters", "results", "rustles", "manes", "manse",
-				"means", "names", "marines", "remains", "seminar", "mast", "mats", "tams", "master", "stream", "tamers",
-				"mate", "meat", "tame", "team", "mates", "meats", "steam", "tames", "teams", "merit", "mitre", "remit",
-				"timer", "merits", "mister", "miters", "mitres", "remits", "timers", "mesa", "same", "seam", "nails",
-				"slain", "snail", "naive", "ravine", "vainer", "nape", "neap", "pane", "naps", "pans", "snap", "span",
-				"nets", "nest", "sent", "tens", "nope", "open", "peon", "pone", "noter", "toner", "tenor", "notes",
-				"onset", "stone", "tones", "now", "own", "won", "observe", "obverse", "verbose", "opt", "pot", "top",
-				"opts", "post", "pots", "stop", "tops", "owns", "snow", "sown", "paled", "pedal", "plead", "palest",
-				"pastel", "petals", "plates", "pleats", "staple", "panel", "penal", "plane", "pares", "parse", "pears",
-				"rapes", "reaps", "spare", "spear", "parley", "pearly", "player", "replay", "parroted", "predator",
-				"prorated", "teardrop", "pars", "raps", "rasp", "spar", "parses", "passer", "spares", "sparse",
-				"spears", "paltry", "partly", "raptly", "parts", "strap", "traps", "parsley", "parleys", "players",
-				"replays", "sparely", "part", "rapt", "trap", "parties", "pastier", "pirates", "traipse", "past",
-				"pats", "spat", "taps", "paste", "peats", "septa", "spate", "tapes", "paws", "swap", "wasp", "pelts",
-				"slept", "spelt", "pertness", "presents", "serpents", "pest", "pets", "step", "piers", "pries", "spire",
-				"pines", "snipe", "spine", "pinto", "piton", "point", "pintos", "piston", "pitons", "points", "pores",
-				"poser", "prose", "ropes", "spore", "potters", "protest", "spotter", "present", "repents", "serpent",
-				"rashes", "shares", "shears", "rattles", "starlet", "startle", "realist", "saltier", "retails",
-				"recused", "reduces", "rescued", "secured", "reigns", "resign", "signer", "singer", "reins", "resin",
-				"rinse", "risen", "siren", "repaint", "painter", "pertain", "repaints", "painters", "pantries",
-				"pertains", "rescue", "recuse", "secure", "resort", "roster", "sorter", "restrain", "retrains",
-				"strainer", "terrains", "trainers", "retests", "setter", "street", "tester", "retrain", "terrain",
-				"trainer", "rite", "tier", "tire", "rites", "tiers", "tires", "tries", "rosiest", "sorties", "stories",
-				"saint", "satin", "stain", "salve", "slave", "vales", "veals", "seated", "sedate", "teased", "serve",
-				"sever", "veers", "verse", "sinew", "swine", "wines", "wisens", "skate", "stake", "steak", "takes",
-				"teaks", "skated", "staked", "tasked", "slates", "steals", "tassel", "state", "taste", "teats", "stew",
-				"wets", "stow", "tows", "twos", "sway", "ways", "yaws", "taster", "tetras", "treats", "thickets",
-				"thickest", "thickset", "weird", "wired", "wider", "whiter", "wither", "writhe" };
-		System.out.println(Arrays.toString(AnagramUtil.getLargestAnagramGroup(word)));
-		System.out.println(Arrays.toString(AnagramUtil.getLargestAnagramGroup(answers)));
-
-	}
-
-	@Test
-	public void testGetLargestAnagramGroupString() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void getLargestAnagramGroupString() {
+	String[] list = { "cat", "Tac", "acT", "Dan", "and", "blink"};
+	String[] expectedList = { "cat", "Tac", "acT" };
+	assertArrayEquals(expectedList, AnagramUtil.getLargestAnagramGroup(list));
+    }
+    
+    @Test
+    public void getLargestAnagramGroupString2() {
+	String[] list = { "Ace", "bad", "Foggry", "reacts", "Even", "Froggy", "recast", "dab" };
+	String[] expectedList = { "Foggry", "Froggy" }; // This might be wrong and probably should be reacts and recast
+	// Waiting for discussion response to see what should be the correct expected list
+	assertArrayEquals(expectedList, AnagramUtil.getLargestAnagramGroup(list));
+    }
+    
+    @Test
+    public void getLargestAnagramGroupString_File(){
+	String[] list = AnagramUtil.getLargestAnagramGroup("sample_word_list.txt");
+	String[] expectedList = { "carets", "Caters", "caster", "crates", "Reacts", "recast", "traces" };
+	assertArrayEquals(expectedList, list);
+    }
+    
 
 }
